240b5640fea658cc3bdc3f3da86bea8bb31ee73a
wgz <wangguozhen5612@163.com>
2017-03-31 14:38:32 +0800

    free the memory

--- stu_info_man.c
+++ stu_info_man.c
@@ -20,17 +20,23 @@ int login(void)
     username = (char *) malloc(sizeof(ori_useranme));
     password = (char *) malloc(sizeof(ori_password));
 
-    puts("please enter your username: ");
-    scanf("%s", username);
+    if ((username != NULL) && (password != NULL)) {
 
-    puts("please enter your password: ");
-    scanf("%s", password);
+        puts("please enter your username: ");
+        scanf("%s", username);
 
-    if ((strcmp(ori_useranme, username) != 0) |
-        (strcmp(ori_password, password) != 0))
-        printf("your username or password is wrong!\n");
-    else
-        puts("login successfully, welcome!");
+        puts("please enter your password: ");
+        scanf("%s", password);
+
+        if ((strcmp(ori_useranme, username) != 0) |
+            (strcmp(ori_password, password) != 0))
+            printf("your username or password is wrong!\n");
+        else
+            puts("login successfully, welcome!");
+    }
+
+    free(username);
+    free(password);
 
     return 0;
 }
