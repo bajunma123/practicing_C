6f0097e64c5450b973130fc899642f57ab59c06c
wgz <wangguozhen5612@163.com>
2017-03-31 14:21:51 +0800

    add the login function

---
+++ stu_info_man.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int login(void);
+
+int main(void)
+{
+    login();
+
+    return 0;
+}
+
+
+int login(void)
+{
+    char *username, *ori_useranme = "abcdef";
+    char *password, *ori_password = "111111";
+
+    username = (char *) malloc(sizeof(ori_useranme));
+    password = (char *) malloc(sizeof(ori_password));
+
+    puts("please enter your username: ");
+    scanf("%s", username);
+
+    puts("please enter your password: ");
+    scanf("%s", password);
+
+    if ((strcmp(ori_useranme, username) != 0) |
+        (strcmp(ori_password, password) != 0))
+        printf("your username or password is wrong!\n");
+    else
+        puts("login successfully, welcome!");
+
+    return 0;
+}
